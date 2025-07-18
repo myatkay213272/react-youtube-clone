117ba38c5234718cad0f38877a51934cc65b8fed
mkzt <myatthant68@gmail.com>
2025-07-18 22:52:48 +0630

    ....

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import {Box} from '@mui/material'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar,Feed,VideoDetail,ChannelDetail,SearchFeed } from './components';
 
 const App = () => (
     <Router>
-        <Box sx={{backgroundColor : '#000'}}>
-            Navbar
+        <Box>
+            <Navbar/>
             <Routes>
-                <Route path="/" exact element={}/>
+                <Route path="/" element={<Feed/>}/>
+                <Route path='/video/:id' element={<VideoDetail/>} />
+                <Route path='/channel/:id' element={<ChannelDetail/>}/>
+                <Route path='/search/:searchTerm' element={<SearchFeed/>}/>
             </Routes>
         </Box>
     </Router>
---
+++ src/components/Feed.jsx
@@ -0,0 +1,9 @@
+const Feed = () => {
+  return (
+    <div>
+      <h1>This is the Feed</h1>
+    </div>
+  );
+};
+
+export default Feed;
