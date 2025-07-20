b04f34689bd6645dcc18374c9565d9930298525a
mkzt <myatthant68@gmail.com>
2025-07-20 22:04:25 +0630

    Create Channel and Video components

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,15 +4,15 @@ import {VideoCard,ChannelCard} from './'
 const Video = ({videos}) => {
   console.log(videos)
   return (
-    <Stack>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
       {videos.map((item,idx)=>(
-        <Box key={item.idx}>
+        <Box key={idx}>
           {item.id.videoId && <VideoCard video={item}/>}
           {item.id.channelId && <ChannelCard channelDetail = {item}/>}
         </Box>
       ))}
     </Stack>
-  )
+  )//
 }
 
 export default Video
